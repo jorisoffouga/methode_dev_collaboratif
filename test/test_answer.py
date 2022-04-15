bf1013453dbe54993f94d913b5a7fba1988cf631
Matteo-Penaud <matteo.penaud@ynov.com>
2022-04-15 10:00:14 +0200

    Tests 1

--- test/test_answer.py
+++ test/test_answer.py
@@ -1,9 +1,18 @@
 def inc(x):
     return x + 1
 
+def sub(x):
+    return x - 1
+
 
 def test_answer():
     assert inc(3) == 5
 
 def test_inc():
     assert inc(3) == 4
+
+def test_sub():
+    assert sub(2) == 1
+
+def test_sub_pt():
+    assert sub(3) == 1
